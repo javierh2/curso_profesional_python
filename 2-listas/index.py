f12f00da168cf46e9b73f124e842d46b392f9bf1
javier <javier.morabaiz@gmail.com>
2023-10-26 17:26:00 -0300

    final del curso

--- 2-listas/index.py
+++ 2-listas/index.py
@@ -2,3 +2,7 @@ lista = [8, 90, 1, 5, 44, 132, 600, 3, 4]
 print(5 in lista)
 indice = lista.index(5)     # retorna el indice en el que se encuentra el elemento de la lista
 print(indice)
+
+listado = [1, 2, 3, 4, 5]
+listita = listado[-3:]
+print(listita)
